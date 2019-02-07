f68c5d50525590c6be128b7e0fb87299916c7da5
alectene <alectene@mail.sfsu.edu>
2019-02-06 19:44:38 -0800

    Finally Finished

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Alec Tenefrancia on 2/6/19.
+//  Copyright © 2019 Alec Tenefrancia. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
